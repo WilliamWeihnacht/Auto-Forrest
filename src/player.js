7cd3d6df22ff948224a77219626812613f0fb702
Will Weihnacht <wwhynot@terpmail.umd.edu>
2022-12-02 16:35:31 -0800

    updated player sprite

--- src/player.js
+++ src/player.js
@@ -1,6 +1,8 @@
 const Util = require("./util");
 
-const S_DIM = 86; //attackSprite dimensions (square)
+//const S_DIM = 86; //attackSprite dimensions (square)
+const WIDTH = 90;
+const HEIGHT = 80;
 
 class Player {
 
@@ -12,33 +14,34 @@ class Player {
         //lifesteal
         this.pos = [20,290];
 
-        this.attackSprite = new Image();
-        this.attackSprite.src = "/Users/wwhynot/Documents/AA homework/JS-Project/assets/player/Knight_1/Attack 1.png"
-        this.attackLoop = [0,1,2,3];
+        this.sprite = new Image();
+        this.sprite.src = "/Users/wwhynot/Documents/AA homework/JS-Project/assets/player/Animated Pixel Knight/knight-sprite-sheet.png";
+
+        // this.attackSprite = new Image();
+        // this.attackSprite.src = "/Users/wwhynot/Documents/AA homework/JS-Project/assets/player/Knight_1/Attack 1.png"
+        this.attackLoop = [0,1,2,3,4];
         this.attackIndex = 0;
         
-        this.idleSprite = new Image();
-        this.idleSprite.src = "/Users/wwhynot/Documents/AA homework/JS-Project/assets/player/Knight_1/Idle.png";
-        this.idleLoop = [0,1,2,3,4];
+        // this.idleSprite = new Image();
+        // this.idleSprite.src = "/Users/wwhynot/Documents/AA homework/JS-Project/assets/player/Knight_1/Idle.png";
+        this.idleLoop = [1,2,3,4];
         this.idleIndex = 0;
     }
 
     draw(ctx) {
-        ctx.beginPath();
-        ctx.drawImage(this.attackSprite,0,0,S_DIM,S_DIM,this.pos[0],this.pos[1],S_DIM,S_DIM);
-        //ctx.drawImage(this.attackSprite,S_DIM,0,S_DIM,S_DIM,this.pos[0],this.pos[1],S_DIM,S_DIM);
+        
     }
 
     animateAttack() {
-        ctx.drawImage(this.attackSprite,S_DIM*this.attackLoop[this.attackIndex],0,S_DIM,S_DIM,this.pos[0],this.pos[1],S_DIM,S_DIM);
+        ctx.drawImage(this.sprite,WIDTH*this.attackLoop[this.attackIndex],HEIGHT,WIDTH,HEIGHT,this.pos[0],this.pos[1],WIDTH,HEIGHT);
         this.attackIndex++;
         if (this.attackIndex >= this.attackLoop.length) this.attackIndex = 0;
     }
 
     animateIdle() {
-        ctx.drawImage(this.idleSprite,S_DIM*this.idleLoop[this.idleIndex],0,S_DIM,S_DIM,this.pos[0],this.pos[1],S_DIM,S_DIM);
-        // this.idleIndex++;
-        // if (this.idleIndex >= this.idleLoop.length) this.idleIndex = 0;
+        ctx.drawImage(this.sprite,WIDTH*this.idleLoop[this.idleIndex],0,WIDTH,HEIGHT,this.pos[0],this.pos[1],WIDTH,HEIGHT);
+        this.idleIndex++;
+        if (this.idleIndex >= this.idleLoop.length) this.idleIndex = 0;
     }
 
     attack(target) {
