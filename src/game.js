978816f0f140141d4b684b1255bb5cbfe4ea2a5e
Will Weihnacht <wwhynot@terpmail.umd.edu>
2023-02-02 15:49:43 -0800

    removed console logs

--- src/game.js
+++ src/game.js
@@ -167,8 +167,8 @@ class Game {
             if (this.player.attackHits()) this.player.attack(this.enemies[0]);
 
             //enemy attacks
-            if (this.enemies[0].attackHits()) console.log(`${this.enemies[0].name} deals ${this.enemies[0].attack(this.player)} damage!`);
-            // if (this.enemies[0].attackHits()) this.enemies[0].attack(this.player);
+            // if (this.enemies[0].attackHits()) console.log(`${this.enemies[0].name} deals ${this.enemies[0].attack(this.player)} damage!`);
+            if (this.enemies[0].attackHits()) this.enemies[0].attack(this.player);
 
             //kill player if their health goes sub 0
             //enemy health handled in their draw method
